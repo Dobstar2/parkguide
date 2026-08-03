3118a5f40298f0d48525844248050a120a834baa
Dobstar2 <128965154+Dobstar2@users.noreply.github.com>
2026-08-03 20:23:43 +0100

    Bump Stinky app cache to version 5

--- stinky/sw.js
+++ stinky/sw.js
@@ -1,5 +1,5 @@
-const CACHE='stinky-checklist-v4';
-const ASSETS=['./','./index.html','./styles.css?v=4','./app.js?v=4','./manifest.webmanifest?v=4','./icon.svg'];
+const CACHE='stinky-checklist-v5';
+const ASSETS=['./','./index.html','./styles.css?v=5','./v4.css?v=5','./app.js?v=5','./manifest.webmanifest?v=5','./icon.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',event=>{
